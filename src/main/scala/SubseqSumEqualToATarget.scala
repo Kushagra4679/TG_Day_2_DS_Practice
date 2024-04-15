8ee4dadfc6d04ec39be1472ab360f7c05ab4462a
Kushagra <kushagrathegame1@gmail.com>
2024-04-15 15:32:59 +0530

    initial commit

--- src/main/scala/SubseqSumEqualToATarget.scala
+++ src/main/scala/SubseqSumEqualToATarget.scala
@@ -1,17 +1,17 @@
 class SubseqSumEqualToATarget {
     def findSubsequencesWithSum(integerArray: Set[Int], target: Int): List[Set[Int]] = {
-      def backtrack(start: Int, currentSum: Int, path: List[Int], answer: List[Set[Int]]): List[Set[Int]] = {
+      def subseqFinder(start: Int, currentSum: Int, path: List[Int], answer: List[Set[Int]]): List[Set[Int]] = {
         if (currentSum == target) {
           answer :+ path.toSet
         } else if (currentSum > target || start >= integerArray.size) {
           answer
         } else {
-          val take = backtrack(start + 1, currentSum + integerArray.toList(start), path :+ integerArray.toList(start), answer)
-          val not_take = backtrack(start + 1, currentSum, path, answer)
+          val take = subseqFinder(start + 1, currentSum + integerArray.toList(start), path :+ integerArray.toList(start), answer)
+          val not_take = subseqFinder(start + 1, currentSum, path, answer)
           take ++ not_take
         }
       }
 
-      backtrack(0, 0, List(), List())
+      subseqFinder(0, 0, List(), List())
     }
   }
